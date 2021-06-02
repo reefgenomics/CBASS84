629b42316b6baf90815a16824e726cc289fa7345
Anny Cárdenas <ajcardenasb1@gmail.com>
2021-06-02 11:00:32 +0200

    Update CBASS84_16S_stats.R

--- CBASS84_16S/CBASS84_16S_stats.R
+++ CBASS84_16S/CBASS84_16S_stats.R
@@ -6,23 +6,22 @@
 library(vegan)
 library(pairwiseAdonis)
 
-otu.ad=as.data.frame(t(read.table("./outputs/CBASS84_noConta_raw.txt", header = TRUE, row.names = 1)))
-otu.n=sweep(otu.ad,2,colSums(otu.ad),`/`) 
+otu.ad=read.table("./outputs/CBASS84_noConta_raw.txt", header = TRUE, row.names = 1)[,1:84]
+otu.n=as.data.frame(t(sweep(otu.ad,2,colSums(otu.ad),`/`)))
 rownames(otu.n)=gsub('\\.', '-', rownames(otu.n))
-map=read.table("./Input_files/metadata.txt", header = TRUE, row.names = 1)
+map=read.table("./Input_files/metadata.txt", header = TRUE, row.names = 1, sep = "\t")
 otu.n$Temperature=map$Temperature[match(rownames(otu.n), rownames(map))] # change colnames by lookup table
 otu.n$Site=map$Region[match(rownames(otu.n), rownames(map))]
-#adonis(otu.ad ~ rownames(map), permutations = 999, method = "bray")
-#adonis(otu.ad  ~ map$Site * map$Experiment2 * map$Temperature, permutations = 999, method = "bray")
+adonis(otu.n[,1:2264] ~ otu.n$Site * otu.n$Temperature, permutations = 999, method = "bray")
 
 #comparing temperatures
-eilat=otu.n[grep("ES", rownames(otu.n)),]
-kaust=otu.n[grep("KS", rownames(otu.n)),]
+ICN=otu.n[grep("ES", rownames(otu.n)),]
+al_fahal=otu.n[grep("KS", rownames(otu.n)),]
 exp=otu.n[grep("-E-", rownames(otu.n)),]
 prot=otu.n[grep("-P-", rownames(otu.n)),]
 
-pairwise.adonis(eilat[,1:2264], eilat$Temperature , p.adjust.m ='fdr', sim.method = 'bray')
-pairwise.adonis(kaust[,1:2264], kaust$Temperature , p.adjust.m ='fdr', sim.method = 'bray')
+pairwise.adonis(ICN[,1:2264], ICN$Temperature , p.adjust.m ='fdr', sim.method = 'bray')
+pairwise.adonis(al_fahal[,1:2264], al_fahal$Temperature , p.adjust.m ='fdr', sim.method = 'bray')
 pairwise.adonis(exp[,1:2264], exp$Temperature , p.adjust.m ='fdr', sim.method = 'bray')
 pairwise.adonis(prot[,1:2264], prot$Temperature , p.adjust.m ='fdr', sim.method = 'bray')
 
